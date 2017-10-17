f3f48da89421618b4f879138c502198589632519
Kankanala,Nikhil Reddy <s528741@nw42844.nwmissouri.edu>
2017-10-16 19:45:12 -0500

    Initial Commit

---
+++ MissouriBirder's/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MissouriBirder's
+//
+//  Created by Kankanala,Nikhil Reddy on 10/16/17.
+//  Copyright © 2017 Kankanala,Nikhil Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
